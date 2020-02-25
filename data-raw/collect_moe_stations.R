062d433480e5a0e3af222e99cbddb6b5134540ab
Shinya Uryu <uryu.shinya@nies.go.jp>
2020-02-25 18:28:10 +0900

    測定局の重複をなくす

--- data-raw/collect_moe_stations.R
+++ data-raw/collect_moe_stations.R
@@ -6,26 +6,24 @@ if (rlang::is_false(file.exists(here::here("data/moe_stations.csv")))) {
   library(rvest)
   library(stringr)
   library(dplyr)
-  
   site_url <- "http://kafun.taiki.go.jp/"
-  
   target_page <- 
     str_c(site_url, "library.html") %>% 
     read_html(encoding = "cp932")
-  
   # 観測地点 --------------------------------------------------------------------
   tidyrup_station_list <- function(df) {
-    
     df_gather <- 
       df %>% 
+      tibble::as_tibble() %>% 
       dplyr::rename(pref = `都道府県`,
                     station = `設置場所`,
                     address = `所在地`,
                     amedas_station = `最寄のアメダス測定局`,
                     note = `備考`) %>% 
-      tidyr::gather("year", "status", -pref, -station, -address, -amedas_station, -note) %>% 
-      tibble::as_tibble()
-    
+      tidyr::pivot_longer(cols = tidyselect::num_range(prefix = "",
+                                                       range = seq.int(2008, 2020)),
+                          names_to = "year",
+                          values_to = "status")
     df_gather %>% 
       dplyr::mutate(status = dplyr::if_else(stringr::str_detect(status, "\u25cb"), TRUE, FALSE)) %>% 
       fuzzyjoin::stringdist_left_join(jpndistrict::jpnprefs %>%
@@ -43,9 +41,7 @@ if (rlang::is_false(file.exists(here::here("data/moe_stations.csv")))) {
                 .funs = list(~ stringr::str_remove_all(., "\t|\n"))) %>% 
       mutate_if(is.character, stringi::stri_trans_nfkc)
   }
-  
   extract_station_list <- function(target_page, table_id) {
-    
     target_page %>% 
       rvest::html_nodes(css = table_id) %>% 
       rvest::html_table(header = TRUE) %>% 
@@ -53,21 +49,21 @@ if (rlang::is_false(file.exists(here::here("data/moe_stations.csv")))) {
         .x = .,
         .f = ~ tidyrup_station_list(.x))
   }
-  
   # extract_station_list(target_page, "#Table9")
-  
   df_moe_stations <- 
     purrr::map(
       .x = paste0("#Table", seq.int(9, 13)),
       .f = ~ extract_station_list(target_page = target_page, .x)) %>% 
     purrr::flatten_dfr() %>% 
-    assertr::verify(dim(.) == c(1440, 7)) %>% 
+    assertr::verify(dim(.) == c(1560, 7)) %>% 
+    distinct(prefecture, station, address, amedas_station, note) %>% 
+    assertr::verify(nrow(.) == 120L) %>% 
     mutate(station_id = row_number()) %>% 
-    select(station_id, everything(), -status, -year)
-  
+    select(station_id, everything())
   df_moe_stations %>% 
     readr::write_csv(here::here("data/moe_stations.csv"))
 } else {
   df_moe_stations <-
-    readr::read_csv(here::here("data/moe_stations.csv"))
+    readr::read_csv(here::here("data/moe_stations.csv"),
+                    col_types = c("dccccc"))
 }
